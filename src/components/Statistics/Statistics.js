7515b48b6e862ceb64f3ed657f14088318bdbcc8
Andrii <scoandrey@gmail.com>
2023-07-03 23:30:58 -0400

    s2

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import './Statistics.css';
 
 class Statistics extends Component {
